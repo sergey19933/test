211a9a319e95962206f2365862ab31accd06cee5
Sergey Churikov <spell1993@yandex.ru>
2021-10-19 20:25:04 +0300

    16

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,7 +1,7 @@
 public class A {
     public static void main(String[] args) {
         System.out.println("m1");
-        System.out.println(12);
+        System.out.println(11);
         System.out.println("sasf");
     }
 }
