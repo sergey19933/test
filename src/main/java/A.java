72c8c6ae755b2068b1ca9f4a47f8cea164ab66db
Sergey Churikov <spell1993@yandex.ru>
2021-10-28 14:54:25 +0300

    23

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -3,8 +3,8 @@ import java.util.Map;
 
 public class A {
     public static void main(String[] args) {
-        Map<B,String>map=new HashMap<>();
-        map.put(new B(5555),"Alla");
+        Map<B, String> map = new HashMap<>();
+        map.put(new B(5555), "Alla");
         System.out.println(map.get(new B(5555)));
         System.out.println("1");
     }
