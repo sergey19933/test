50f2a8a7dd5f61b0c577740e6415d1e69925f03e
“ChurikovSA” <spell1993@yandex.ru>
2021-11-26 19:14:49 +0300

    336

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -6,10 +6,5 @@ public class A {
         Map<B,String>map=new HashMap<>();
         map.put(new B(5555),"Alla");
         System.out.println(map.get(new B(5555)));
-        System.out.println("1");
-        System.out.println("sdgdfgdf");
-        System.out.println("sdgdfgdf");
-        //фвп
-        //ывпвп
     }
 }
