75d6fda7a90bcf5608e15a6da0c0b645ed9e4efc
Сергей <66200989+sergey19933@users.noreply.github.com>
2022-04-19 19:55:00 +0300

    Update Solution.java
    fgj

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -8,6 +8,7 @@ public class Solution {
     public static int countNot = 0;
     public static int count = 0;
 // new version
+    //sdgsfhdfhd
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bufferedReader.readLine());
